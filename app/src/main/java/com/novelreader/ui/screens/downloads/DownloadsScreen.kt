c392f17bf7a2eaf688d041b205474ca7810fcd76
elfred434 <elfred434@gmail.com>
2026-07-08 21:46:49 +0100

    Fix: import DownloadItem dans DownloadsScreen

--- app/src/main/java/com/novelreader/ui/screens/downloads/DownloadsScreen.kt
+++ app/src/main/java/com/novelreader/ui/screens/downloads/DownloadsScreen.kt
@@ -40,6 +40,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.novelreader.data.download.DownloadItem
 import com.novelreader.data.download.DownloadStatus
 import com.novelreader.ui.components.EmptyView
 
